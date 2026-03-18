90f7c390d454bd50b947d40df5899be5606d7078
Ted Neward <ted@tedneward.com>
2026-03-17 23:43:01 -0700

    I think Chirper is now localhost-clustered but remote-accessible, but this is untested as of yet.

--- Chirper/Chirper.Client/Program.cs
+++ Chirper/Chirper.Client/Program.cs
@@ -2,12 +2,16 @@ using Chirper.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 Console.Title = "Chirper Client";
 
 await new HostBuilder()
     .UseOrleansClient(builder =>
-        builder.UseLocalhostClustering())
+        builder.UseStaticClustering(new IPEndPoint[]
+        {
+            IPEndPoint.Parse("172.22.37.222:30000")
+        }))
     .ConfigureServices(
         services => services
             .AddSingleton<IHostedService, ShellHostedService>()
--- Chirper/Chirper.Server/Program.cs
+++ Chirper/Chirper.Server/Program.cs
@@ -1,24 +1,29 @@
 using Microsoft.Extensions.Hosting;
+using Orleans;
 using Orleans.Configuration;
+using Orleans.Hosting;
 using System.Net;
 
 Console.Title = "Chirper Server";
 
 await Host.CreateDefaultBuilder(args)
-    .UseOrleans(
-        builder => builder
-        .Configure<EndpointOptions>(options =>
+    .UseOrleans(silo =>
+    {
+        silo.UseLocalhostClustering(
+            siloPort: 11111,
+            gatewayPort: 30000,
+            primarySiloEndpoint: new IPEndPoint(IPAddress.Loopback, 11111));
+
+        silo.Configure<ClusterOptions>(options =>
         {
-            // Port to use for silo-to-silo
-            options.SiloPort = 11_111;
-            // Port to use for the gateway
-            options.GatewayPort = 30_000;
-            // IP Address to advertise in the cluster
-            options.AdvertisedIPAddress = IPAddress.Parse("192.168.0.114");
-            // The socket used for client-to-silo will bind to this endpoint
-            options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 40_000);
-            // The socket used by the gateway will bind to this endpoint
-            options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 50_000);
-        })
-        .AddMemoryGrainStorage("AccountState"))
+            options.ClusterId = "dev";
+            options.ServiceId = "game";
+        });
+
+        silo.Configure<SiloOptions>(opts =>
+        {
+            opts.SiloName = "silo-0";
+        });
+    })
     .RunConsoleAsync();
+
